5046f8714c8f75429186ffd5e210663d991147e3
Elvis de Freitas <edigitalb@gmail.com>
2017-10-21 12:42:04 -0200

    simplify map creation

--- cmd/main.go
+++ cmd/main.go
@@ -11,15 +11,15 @@ func main(){
 	http.HandleFunc("/students", func(w http.ResponseWriter, r *http.Request) {
 
 		w.Header().Set("Content-Type", "application/json")
-		var students []map[string]interface{}
-		students = append(students, map[string]interface{}{
-			"name": "Elvis",
-			"age": 22,
-		},
-		map[string]interface{}{
-			"name": "Bruna",
-			"age": 21,
-		})
+		students := []map[string]interface{}{ map[string]interface{}{
+				"name": "Elvis",
+				"age": 22,
+			},
+			map[string]interface{}{
+				"name": "Bruna",
+				"age": 21,
+			}}
+
 		if err := json.NewEncoder(w).Encode(students); err != nil {
 			w.WriteHeader(http.StatusBadRequest)
 		}
